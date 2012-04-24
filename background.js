da19240dfd84a2b59d491e43bd10e6ea4989a61d
Eli Horne <elihorne@gmail.com>
2012-04-24 12:08:12 -0400

    first commmit

---
+++ background.js
@@ -0,0 +1,13 @@
+function rdify() {
+	rdiosearch = 'http://www.rdio.com/search/';
+	url = window.location.href;
+	if(url.indexOf('track') > -1 || url.indexOf('album') > -1) {
+		target = $('a#title').text();
+		window.location.href = rdiosearch + encodeURIComponent(target);
+	}
+}
+
+rdify();
+
+// album: http://open.spotify.com/album/3hVR63Y2ElBoxmzThy80nG
+// track: http://open.spotify.com/track/5MuWU8UkuUpmQMVgLqkEIb
---
+++ readme.md
@@ -0,0 +1,3 @@
+A simple Chrome extension to forward open.spotify links over to [RDIO](http://rdio.com).
+
+[Download and Install](http://x.elihorne.com/chrome/rdify)
